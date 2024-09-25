1600f7eaf563b3265b9b697c9a4470dafea677cc
Enrico Talmelli <enrico.talmelli@gmail.com>
2024-09-25 13:09:57 +0200

    fixed conventions and warning suggested by pylint

--- list_scheduling/__init__.py
+++ list_scheduling/__init__.py
@@ -1,4 +1,8 @@
-from .list_scheduling import main, setup_parser
+"""__init__ file of the project"""
+from . import list_scheduling
 
 def main():
+    """
+    main function of __init__ file. It calls the main function in list_scheduling.py
+    """
     print(list_scheduling.main(list_scheduling.setup_parser()))
--- list_scheduling/parser.py
+++ list_scheduling/parser.py
@@ -1,3 +1,13 @@
+"""
+Module for processing configuration file and setting up command-line arguments for the list scheduling algorithm.
+
+Functions:
+----------
+1. process_file(file_path)
+
+2. setup_parser():
+"""
+
 import argparse
 from list_scheduling.operation import Operation
 
@@ -31,7 +41,7 @@ def process_file(file_path):
         If a line does not conform to the expected format or contains invalid arguments.
     """
     try:
-        with open(file_path, 'r') as file:
+        with open(file_path, 'r', encoding="utf-8") as file:
             array_operations = []
             for line_num, line in enumerate(file, start=1):
 
@@ -41,35 +51,35 @@ def process_file(file_path):
                 # ignore comments and empty lines
                 if (line.startswith('#') or not line):
                     continue
-                
+
                 # split the line into parts
                 parts = line.split(" ")
 
                 # every line must contain 5 elements:
-                if (len(parts) > 5):
+                if len(parts) > 5:
                     raise ValueError(f"Error in line {line_num}: too many arguments")
-                
+
                 # operations name must starts with letter 'u'
-                if (not parts[0].startswith("u")):
+                if not parts[0].startswith("u"):
                     raise ValueError(f"Error in line {line_num}: operation {parts[0]} must start with the letter 'u'")
-                
+
                 # the delimiter between the operation name and the operands must be ":="
-                if (not parts[1] == ":="):
+                if not parts[1] == ":=":
                     raise ValueError(f"Error in line {line_num}: operation misspelled ")
-                
+
                 # check allowed types of operation
                 if (not parts[3] in ['+', '-', '*', '/']):
                     raise ValueError(f"Error in line {line_num}: operation allowed are only + - * /")
-                
+
                 # create a new Operation object
                 new_operation = Operation(parts[0], parts[3], parts[2], parts[4])
                 if new_operation:
                     array_operations.append(new_operation)
-        
+
         return array_operations
 
-    except FileNotFoundError:
-        print(f"Error. File {file_path} non found")
+    except FileNotFoundError as e:
+        raise ValueError(f"Error. File {file_path} not found") from e  # Raise error
 
 def setup_parser() -> argparse.Namespace:
     """
@@ -100,5 +110,5 @@ def setup_parser() -> argparse.Namespace:
     # arguments for numbers of multipliers and adders
     parser.add_argument("--nmult", type=int, help="Number of multipliers available", required=True)
     parser.add_argument("--nadd", type=int, help="Number of adders available", required=True)
-    
+
     return parser.parse_args()
